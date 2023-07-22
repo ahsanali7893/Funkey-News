1285a4c7b014368a3ffcb53793ef348ce321f747
ahsanali7893 <ahsan3916902@gmail.com>
2023-07-22 20:51:44 +0500

    initial changes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -14,25 +14,9 @@ const Navbar = (props: Props) => {
             <div className="flex md:order-2">
               <div className="relative mr-3 md:mr-0 hidden md:block">
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <svg
-                    className="w-5 h-5 text-gray-500"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
+                
                 </div>
-                <input
-                  type="text"
-                  id="email-adress-icon"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2"
-                  placeholder="Search..."
-                />
+              
               </div>
               <button
                 data-collapse-toggle="mobile-menu-3"
@@ -73,6 +57,19 @@ const Navbar = (props: Props) => {
               id="mobile-menu-3"
             >
               <ul className="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
+                
+                 
+                    <Select
+                      label=""
+                      placeholder="Pick one"
+                      data={[
+                        { value: "react", label: "React" },
+                        { value: "ng", label: "Angular" },
+                        { value: "svelte", label: "Svelte" },
+                        { value: "vue", label: "Vue" },
+                      ]}
+                    />
+                  
                 <li>
                   <a
                     href="#"
@@ -98,23 +95,6 @@ const Navbar = (props: Props) => {
                     Services
                   </a>
                 </li>
-                <li>
-                  <a
-                    href="#"
-                    className="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0"
-                  >
-                    <Select
-                      label=""
-                      placeholder="Pick one"
-                      data={[
-                        { value: "react", label: "React" },
-                        { value: "ng", label: "Angular" },
-                        { value: "svelte", label: "Svelte" },
-                        { value: "vue", label: "Vue" },
-                      ]}
-                    />
-                  </a>
-                </li>
               </ul>
             </div>
           </div>
@@ -126,7 +106,7 @@ const Navbar = (props: Props) => {
             href="#"
             target="_blank"
           ></a>
-          .
+          
         </p>
       </div>
 
