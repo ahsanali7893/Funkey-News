6b281d6d2af59a1fb3a318356a438545d95c8a1b
ahsanali7893 <ahsan3916902@gmail.com>
2023-07-23 14:22:20 +0500

    inital changes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Select } from "@mantine/core";
+
 
 type Props = {};
 
@@ -6,24 +6,36 @@ const Navbar = (props: Props) => {
   return (
     <>
       <div className="max-w-4xl mx-auto py-3">
-      <header className="text-gray-600 body-font">
-  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-      <span className="ml-3 text-xl">Tailblocks</span>
-    </a>
-    <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-      <a className="mr-5 hover:text-gray-900">First Link</a>
-      <a className="mr-5 hover:text-gray-900">Second Link</a>
-      <a className="mr-5 hover:text-gray-900">Third Link</a>
-      <a className="mr-5 hover:text-gray-900">Fourth Link</a>
-    </nav>
-    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-        <path d="M5 12h14M12 5l7 7-7 7"></path>
-      </svg>
-    </button>
-  </div>
-</header>
+        <header className="text-gray-600 body-font">
+          <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+            <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+              <span className="ml-3 text-xl">Funkey-News</span>
+            </a>
+            <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center list-none ">
+              <li className="nav-item px-2">
+                  Business
+              </li>
+              <li className="nav-item">
+                  Entertainment
+              </li>
+              <li className="nav-item">
+                  General
+              </li>
+              <li className="nav-item">
+                  Health
+              </li>
+              <li className="nav-item">
+                  Science
+              </li>
+              <li className="nav-item">
+                  Sports
+              </li>
+              <li className="nav-item">
+                  Technology
+              </li>
+            </nav>
+          </div>
+        </header>
 
         <p className="mt-5">
           <a
