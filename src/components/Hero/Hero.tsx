fda6cb8d0e30705a0bbec67acce922d255b46ec0
khalilahmed63 <panhwarkhalilahmed@gmail.com>
2023-07-27 13:54:16 +0500

    console added

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,4 +1,4 @@
-import { IconBookmark, IconHeart, IconShare } from '@tabler/icons-react';
+import { IconBookmark, IconHeart, IconShare } from "@tabler/icons-react";
 import {
   Card,
   Image,
@@ -10,33 +10,40 @@ import {
   Avatar,
   createStyles,
   rem,
-} from '@mantine/core';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+} from "@mantine/core";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const useStyles = createStyles((theme) => ({
   card: {
-    position: 'relative',
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+    position: "relative",
+    backgroundColor:
+      theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
   },
 
   rating: {
-    position: 'absolute',
+    position: "absolute",
     top: theme.spacing.xs,
     right: rem(12),
-    pointerEvents: 'none',
+    pointerEvents: "none",
   },
 
   title: {
-    display: 'block',
+    display: "block",
     marginTop: theme.spacing.md,
     marginBottom: rem(5),
   },
 
   action: {
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+    backgroundColor:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[6]
+        : theme.colors.gray[0],
     ...theme.fn.hover({
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1],
+      backgroundColor:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[5]
+          : theme.colors.gray[1],
     }),
   },
 
@@ -66,18 +73,23 @@ export function Hero({
   author,
   rating,
   ...others
-}:any& ArticleCardProps & Omit<React.ComponentPropsWithoutRef<'div'>, keyof ArticleCardProps>) {
+}: any &
+  ArticleCardProps &
+  Omit<React.ComponentPropsWithoutRef<"div">, keyof ArticleCardProps>) {
   const { classes, cx, theme } = useStyles();
-  const linkProps = { href: link, target: '_blank', rel: 'noopener noreferrer' };
-
+  const linkProps = {
+    href: link,
+    target: "_blank",
+    rel: "noopener noreferrer",
+  };
 
   const fetchArticleAPI = process.env.REACT_APP_API_Article;
-
+  console.log(fetchArticleAPI, "fetchArticleAPI");
   const fetchRecord = async () => {
     try {
-      const response = await axios.get(`${fetchArticleAPI}?page=${page}`);
+      const response = await axios.get(`${fetchArticleAPI}`);
       setRecord(response?.data);
-      console.log(response, response);
+      console.log(response, "response");
     } catch (error) {
       console.log(error);
     }
@@ -92,10 +104,15 @@ export function Hero({
 
   return (
     <>
-   {record.map((article:any) => (
-        <div key={article.id} className='max-w-6xl mx-auto'>
-          <div className='py-44 w-64'>
-            <Card withBorder radius='sm' className={cx(classes.card, className)} {...others}>
+      {record.map((article: any) => (
+        <div key={article.id} className="max-w-6xl mx-auto">
+          <div className="py-44 w-64">
+            <Card
+              withBorder
+              radius="sm"
+              className={cx(classes.card, className)}
+              {...others}
+            >
               <Card.Section>
                 <a {...linkProps}>
                   <Image src={article.image} height={180} />
@@ -104,37 +121,47 @@ export function Hero({
 
               <Badge
                 className={classes.rating}
-                variant='gradient'
-                gradient={{ from: 'yellow', to: 'red' }}
+                variant="gradient"
+                gradient={{ from: "yellow", to: "red" }}
               >
                 {article.rating}
               </Badge>
 
-              <Text className={classes.title} fw={500} component='a' {...linkProps}>
+              <Text
+                className={classes.title}
+                fw={500}
+                component="a"
+                {...linkProps}
+              >
                 {article.title}
               </Text>
 
-              <Text fz='sm' color='dimmed' lineClamp={4}>
+              <Text fz="sm" color="dimmed" lineClamp={4}>
                 {article.description}
               </Text>
 
-              <Group position='apart' className={classes.footer}>
+              <Group position="apart" className={classes.footer}>
                 <Center>
-                  <Avatar src={article.author.image} size={24} radius='xl' mr='xs' />
-                  <Text fz='sm' inline>
+                  <Avatar
+                    src={article.author.image}
+                    size={24}
+                    radius="xl"
+                    mr="xs"
+                  />
+                  <Text fz="sm" inline>
                     {article.author.name}
                   </Text>
                 </Center>
 
                 <Group spacing={8} mr={0}>
                   <ActionIcon className={classes.action}>
-                    <IconHeart size='1rem' color={theme.colors.red[6]} />
+                    <IconHeart size="1rem" color={theme.colors.red[6]} />
                   </ActionIcon>
                   <ActionIcon className={classes.action}>
-                    <IconBookmark size='1rem' color={theme.colors.yellow[7]} />
+                    <IconBookmark size="1rem" color={theme.colors.yellow[7]} />
                   </ActionIcon>
                   <ActionIcon className={classes.action}>
-                    <IconShare size='1rem' />
+                    <IconShare size="1rem" />
                   </ActionIcon>
                 </Group>
               </Group>
