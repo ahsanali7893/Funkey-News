343d15f610bac192cb2b23b058f903c91a5f26d4
ahsanali7893 <ahsan3916902@gmail.com>
2023-08-08 21:09:20 +0500

    api change

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -73,14 +73,16 @@ export function Hero({
   author,
   rating,
   ...others
-}: any & ArticleCardProps & Omit<React.ComponentPropsWithoutRef<"div">, keyof ArticleCardProps>) {
+}: any &
+  ArticleCardProps &
+  Omit<React.ComponentPropsWithoutRef<"div">, keyof ArticleCardProps>) {
   const { classes, cx, theme } = useStyles();
   const linkProps = {
     href: link,
     target: "_blank",
     rel: "noopener noreferrer",
   };
-  const articlesPerPage = 6; 
+  const articlesPerPage = 6;
 
   const fetchArticleAPI = process.env.REACT_APP_API_Articles;
   console.log(fetchArticleAPI, "fetchArticleAPI");
@@ -88,7 +90,7 @@ export function Hero({
     try {
       const response = await axios.get(`${fetchArticleAPI}`);
       setRecord(response?.data.articles);
-      setPageCount(response?.data.total_pages);   
+      setPageCount(response?.data.total_pages);
       setRecord((prevRecord) => [...prevRecord, fetchArticleAPI]);
       console.log(response, "response");
     } catch (error) {
@@ -115,80 +117,93 @@ export function Hero({
       <div style={{ display: "flex", flexWrap: "wrap" }}>
         {record && record.length > 0 ? (
           record
-          .slice((page - 1) * articlesPerPage, page * articlesPerPage)
-          .map((article: any) => (
-            <div key={article.id} className="w-full md:w-1/3 px-32">
-              <div className="pt-44 w-64 ">
-                <Card
-                  withBorder
-                  radius="sm"
-                  className={cx(classes.card, className)}
-                  {...others}
-                >
-                  <Card.Section>
-                    <a {...linkProps}>
-                      <Image src={article.urlToImage} height={180} />
-                    </a>
-                  </Card.Section>
-
-                  <Badge
-                    className={classes.rating}
-                    variant="gradient"
-                    gradient={{ from: "yellow", to: "red" }}
+            .slice((page - 1) * articlesPerPage, page * articlesPerPage)
+            .map((article: any) => (
+              <div key={article.id} className="w-full md:w-1/3 px-32">
+                <div className="pt-44 w-64 ">
+                  <Card
+                    withBorder
+                    radius="sm"
+                    className={cx(classes.card, className)}
+                    {...others}
                   >
-                    {article.rating}
-                  </Badge>
+                    <Card.Section>
+                      <a {...linkProps}>
+                        <Image src={article.urlToImage} height={180} />
+                      </a>
+                    </Card.Section>
 
-                  <Text
-                    className={classes.title}
-                    fw={500}
-                    component="a"
-                    {...linkProps}
-                  >
-                    {article.title}
-                  </Text>
+                    <Badge
+                      className={classes.rating}
+                      variant="gradient"
+                      gradient={{ from: "yellow", to: "red" }}
+                    >
+                      {article.rating}
+                    </Badge>
 
-                  <Text fz="sm" color="dimmed" lineClamp={4}>
-                    {article.description}
-                  </Text>
+                    <Text
+                      className={classes.title}
+                      fw={500}
+                      component="a"
+                      {...linkProps}
+                    >
+                      {article.title}
+                    </Text>
 
-                  <Group position="apart" className={classes.footer}>
-                    <Center>
-                      {article.author?.urlToImage ? (
-                        <Avatar
-                          src={article.author.urlToImage}
-                          size={24}
-                          radius="xl"
-                          mr="xs"
-                        />
-                      ) : null}
-                      <Text fz="sm" inline>
-                        {article.author?.name}
-                      </Text>
-                    </Center>
+                    <Text fz="sm" color="dimmed" lineClamp={4}>
+                      {article.description}
+                    </Text>
 
-                    <Group spacing={8} mr={0}>
-                      <ActionIcon className={classes.action}>
-                        <IconHeart size="1rem" color={theme.colors.red[6]} />
-                      </ActionIcon>
-                      <ActionIcon className={classes.action}>
-                        <IconBookmark size="1rem" color={theme.colors.yellow[7]} />
-                      </ActionIcon>
-                      <ActionIcon className={classes.action}>
-                        <IconShare size="1rem" />
-                      </ActionIcon>
+                    <Group position="apart" className={classes.footer}>
+                      <Center>
+                        {article.author?.urlToImage ? (
+                          <Avatar
+                            src={article.author.urlToImage}
+                            size={24}
+                            radius="xl"
+                            mr="xs"
+                          />
+                        ) : null}
+                        <Text fz="sm" inline>
+                          {article.author?.name}
+                        </Text>
+                      </Center>
+
+                      <Group spacing={8} mr={0}>
+                        <ActionIcon className={classes.action}>
+                          <IconHeart size="1rem" color={theme.colors.red[6]} />
+                        </ActionIcon>
+                        <ActionIcon className={classes.action}>
+                          <IconBookmark
+                            size="1rem"
+                            color={theme.colors.yellow[7]}
+                          />
+                        </ActionIcon>
+                        <ActionIcon className={classes.action}>
+                          <IconShare size="1rem" />
+                        </ActionIcon>
+                      </Group>
                     </Group>
-                  </Group>
-                </Card>
+                  </Card>
+                </div>
               </div>
-            </div>
-          ))
+            ))
         ) : (
           <p className="text-center py-32 mx-auto">No articles found.</p>
         )}
         <div className="w-full mt-4 px-2 flex justify-between max-w-screen-2xl mx-auto">
-          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" onClick={handlePreviousPage}>Previous</button>
-          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" onClick={handleNextPage}>Next</button>
+          <button
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+            onClick={handlePreviousPage}
+          >
+            Previous
+          </button>
+          <button
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+            onClick={handleNextPage}
+          >
+            Next
+          </button>
         </div>
       </div>
     </>
