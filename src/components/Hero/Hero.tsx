4213a4bf0228b948c265984cc764ed3b5aa5f03e
ahsanali7893 <ahsan3916902@gmail.com>
2023-08-13 12:57:11 +0500

    initial changes

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -125,7 +125,7 @@ export function Hero({
                       rel="noopener noreferrer"
                       className="text-blue-500 hover:underline"
                     >
-                      {article.title.split(' ').slice(0, 2).join(' ')}
+                      {article.title.split(' ').slice(0, 2).join(' ')}...
                     </a>
                   </h3>
 
